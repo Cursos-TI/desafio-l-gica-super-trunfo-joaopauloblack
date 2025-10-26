718083768e3367ffdad3d4215f2fa6408a78ebf3
joaopauloblack <joaopauloblack29@gmail.com>
2025-10-26 19:00:42 +0000

    Edição Desafio Lógica Super Trunfo

---
+++ Untitled-1.c
@@ -0,0 +1,107 @@
+// Desafio Lógica Super Trunfo - Países
+// Tema 2- Comparação das Cartas
+
+
+#include <stdio.h>
+
+int main(){
+
+}
+
+int main() {
+
+    Carta 1
+
+    String estado;
+    String código;
+    String nome da cidade;
+    int    população;
+    float  área;
+    float  PIB;
+    int    pontos turísticos;
+
+    Carta 2
+
+    String estado;
+    String codigo;
+    String nome da cidade;
+    int    população;
+    float  área;
+    float  PIB;
+    int    pontos turísticos;
+
+
+}
+
+    Carta 1
+
+    printf ("Digite o Estado: ");
+    scanf ("%s", &estado);
+    printf ("Digite o codigo: ");
+    scanf ("%s", &codigo);
+    printf ("Digite o nome da cidade: ");
+    scanf ("%s", &nomedacidade);
+    printf ("Digite a população);
+    scanf ("%d", &população);
+    printf ("Digite a area: ");
+    scanf ("%f", &area);
+    printf ("Digite o PIB: ");
+    scanf ("%f", &PIB);
+    printf ("Digite os pontos turisticos: ");
+    scanf ("%d", &pontosturisticos);
+
+
+    
+    Carta 2
+
+    printf ("Digite o Estado: ");
+    scanf ("%s", &estado);
+    printf ("Digite o codigo: ");
+    scanf ("%s", &codigo);
+    printf ("Digite o nome da cidade: ");
+    scanf ("%s", &nomedacidade);
+    printf ("Digite a população);
+    scanf ("%d", &população);
+    printf ("Digite a area: ");
+    scanf ("%f", &area);
+    printf ("Digite o PIB: ");
+    scanf ("%f", &PIB);
+    printf ("Digite os pontos turisticos: ");
+    scanf ("%d", &pontosturisticos);
+
+ 
+
+    int main ()
+
+    int "%d";
+
+    Carta  1
+    
+    printf ("Digite a população: ");
+    scanf ("%d" ,&população);
+
+    Carta 2 
+
+    printf ("Digite a população: ");
+    scanf ("%d" ,&população);
+
+
+    If ("PopulaçãoA > PopulaçãoB"){
+    Printf ("Cidade 1 tem a maior população. \n");
+    }else {
+    If ("PopulaçãoB > PopulaçãoA");
+    printf ("Cidade 2 tem a maior população. \n");
+    } 
+    
+
+    printf ("A Cidade Vencedora é: %s\n", cidadevencedora);
+
+   
+   Comparação de Cartas (Atributo: População):
+
+   Carta 1 São Paulo (SP) 11.904.961
+   Carta 2 Rio de Janeiro (RJ) 6.730.729
+
+   Resultado: Carta 1 São Paulo, Venceu.
+
+   return 0;
